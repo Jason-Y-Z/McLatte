017f29944c4ad3f7fd13e34f8bda4d76294d209a
JasonYZ <yurenzhang2017@gmail.com>
2021-12-11 14:00:38 +0000

    MODEL: Implement baseline RNN model and training script

---
+++ rnn/dataset.py
@@ -0,0 +1,95 @@
+""" 
+Dataset and Dataloader designed for time series predictions.
+"""
+# Author: Jason Zhang (yurenzhang2017@gmail.com)
+# License: BSD 3 clause
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from typing import Optional
+
+
+class ShiftingDataset(Dataset):
+    """
+    This dataset takes an input time series, shifting the timestep by 1, 
+    and use that as the target for prediction.
+    """
+    def __init__(self, Y: np.array, seq_len: int) -> None:
+        super().__init__()
+
+        self._Y = Y
+        self._n_subject = Y.shape[0]
+        self._seq_len = seq_len
+    
+    def __len__(self) -> int:
+        return (self._Y.shape[1] - self._seq_len) * self._n_subject
+    
+    def __getitem__(self, idx):
+        """
+        The indexing enumerates the subject dimension first 
+        and the time dimension second.
+        """
+        time_start = idx // self._n_subject
+        time_end = time_start + self._seq_len
+        subject_idx = idx % self._n_subject
+        return (
+            torch.from_numpy(
+                self._Y[subject_idx, time_start:time_end]
+            ).unsqueeze(1).float(),
+            torch.from_numpy(
+                self._Y[subject_idx, time_start + 1:time_end + 1]
+            ).unsqueeze(1).float()
+        )
+
+
+class ShiftingDataModule(pl.LightningDataModule):
+    def __init__(self, Y: np.array, seq_len: int, batch_size: int):
+        super().__init__()
+
+        self._Y = Y
+        self._seq_len = seq_len
+        self._batch_size = batch_size
+    
+    def setup(self, stage: Optional[str] = None) -> None:
+        if stage in (None, "fit"):
+            full_dataset = ShiftingDataset(
+                self._Y,
+                self._seq_len,
+            )
+
+            seq_length = len(full_dataset)
+            self._train_dataset, self._valid_dataset = random_split(
+                full_dataset, 
+                [round(seq_length * 0.8), round(seq_length * 0.2)]
+            )
+    
+    def train_dataloader(self):
+        return DataLoader(
+            self._train_dataset, 
+            batch_size=self._batch_size, 
+            shuffle=True,
+            num_workers=16,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self._valid_dataset, 
+            batch_size=self._batch_size,
+            num_workers=4,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self._valid_dataset, 
+            batch_size=self._batch_size,
+            num_workers=4,
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self._valid_dataset, 
+            batch_size=self._batch_size,
+            num_workers=4,
+        )
---
+++ tests/test_baseline_rnn.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import pytest
+import torch
+from mclatte.simulation_data import generate_simulation_data, TreatmentRepr
+from pytorch_lightning import Trainer
+from rnn.model import BaselineRnn
+from rnn.dataset import ShiftingDataModule
+
+
+# Given
+@pytest.mark.parametrize('N', (20, ))
+@pytest.mark.parametrize('M', (100, ))
+@pytest.mark.parametrize('H', (100, ))
+@pytest.mark.parametrize('R', (1, ))
+@pytest.mark.parametrize('D', (10, ))
+@pytest.mark.parametrize('K', (5, ))
+@pytest.mark.parametrize('C', (5, ))
+@pytest.mark.parametrize(
+    'mode', 
+    (
+        TreatmentRepr.BINARY, 
+    )
+)
+def test_mclatte(N, M, H, R, D, K, C, mode):
+    _, _, Y_pre, Y_post, _, _ = generate_simulation_data(N, M, H, R, D, K, C, mode)
+    data_module = ShiftingDataModule(
+        Y=np.concatenate((Y_pre, Y_post), axis=1),
+        seq_len=16,
+        batch_size=8,
+    )
+    rnn = BaselineRnn(
+        rnn=torch.nn.RNN(input_size=1, hidden_size=4), 
+        hidden_dim=4,
+        output_dim=1,
+        lr=1e-2, 
+        gamma=0.9, 
+    )
+    trainer = Trainer(
+        default_root_dir=os.path.join(os.getcwd(), 'tests/data'),
+        max_epochs=1,
+        progress_bar_refresh_rate=0,
+        gpus=1,
+        log_every_n_steps=1,
+    )
+    trainer.fit(rnn, data_module)
